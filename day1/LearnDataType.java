ed79734be5438508231152ef0a8f006f5ee0017c
SriVino846 <56034269+SriVino846@users.noreply.github.com>
2025-04-27 14:46:18 +0530

    Add files via upload

---
+++ day1/LearnDataType.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class LearnDataType {
+	
+	public static void main(String[] args) {
+		
+		//integer
+		
+		//Primitive Data Type
+		
+		byte age = 32;
+		System.out.println(age);
+		
+		int noOfLearners = 1234567892;
+		
+		long phNo=123456789L;
+		
+		short regNo = 12345;
+		
+		//Non-Primitive Data Type
+		
+		String name = "Sri";
+		System.out.println(name);
+	}
+
+}
