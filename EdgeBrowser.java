f800e5459115196e88081f189d335f12199a51d5
SriVino846 <56034269+SriVino846@users.noreply.github.com>
2025-05-02 21:57:37 +0530

    Add files via upload

---
+++ EdgeBrowser.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Browser bro = new Browser();
+		
+		bro.launchBrowser("edge");
+		bro.loadUrl();
+		
+	}
+
+}
